58b4d6bc7af643095683c022d0716d3ee7c0f8cb
mahendra_singh <mahendra_singh@seologistics.com>
2018-06-12 10:37:23 +0530

    remove source code

--- src/app/angular2-material-icons/svg-icons.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SvgIconsDirective } from './svg-icons.directive';
-
-describe('SvgIconsDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SvgIconsDirective();
-    expect(directive).toBeTruthy();
-  });
-});
